6b7d4c23cfd6947a37f4c29afbe6141b3e7dc2c7
porgetit <kevin.esguerra@utp.edu.co>
2023-11-13 17:14:01 -0500

    Create porject Futbol5

---
+++ Futbol5/src/main/java/com/mycompany/futbol5/Futbol5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.futbol5;
+
+/**
+ *
+ * @author xdpor
+ */
+public class Futbol5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
